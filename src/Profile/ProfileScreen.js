64220b39b300791b087929758a3f8d045a2087ed
Dhruv <dhruv.rastogi@digichorus.com>
2020-06-15 12:15:26 +0530

    Profile screen UI revamp by Dhruv Rastogi

--- src/Profile/ProfileScreen.js
+++ src/Profile/ProfileScreen.js
@@ -10,36 +10,10 @@ export default class ProfileScreen extends Component {
   }
   componentDidMount() {}
 
-  onClickListener = viewId => {
-    if (viewId === 'profile') {
-      this.props.navigation.dispatch(StackActions.replace('Profile'));
-    } else if (viewId === 'notifications') {
-      this.props.navigation.dispatch(StackActions.replace('Notification'));
-    } else if (viewId === 'offer') {
-      this.props.navigation.dispatch(StackActions.replace('Offers'));
-    }
-  };
-
   render() {
     return (
-      <View style={{flex: 5}}>
-        <View style={{flex: 4}}>
-          <Text>LIST</Text>
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <TouchableOpacity
-            onPress={() => this.onClickListener('offer')}
-            style={{flex: 1, backgroundColor: 'red'}}
-          />
-          <TouchableOpacity
-            onPress={() => this.onClickListener('profile')}
-            style={{flex: 1, backgroundColor: 'green'}}
-          />
-          <TouchableOpacity
-            onPress={() => this.onClickListener('notifications')}
-            style={{flex: 1, backgroundColor: 'blue'}}
-          />
-        </View>
+      <View>
+        <Text>Dhruv</Text>
       </View>
     );
   }
