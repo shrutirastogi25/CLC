8468c3e44c49747e129155922944f733d15046d8
shrutirastogi25 <shruti.rastogi2501@gmail.com>
2020-06-19 19:05:05 +0530

    Changes done in ProfileScreen by Shruti

--- src/Profile/ProfileScreen.js
+++ src/Profile/ProfileScreen.js
@@ -1,7 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {StackActions} from '@react-navigation/native';
+
 // import { moderateScale, isTabDevice, isiPhoneX } from '../reusableComponents/DeviceInfo';
 
 export default class ProfileScreen extends Component {
@@ -9,36 +11,63 @@ export default class ProfileScreen extends Component {
     super(props);
   }
   componentDidMount() {}
-
-  onClickListener = viewId => {
-    if (viewId === 'profile') {
-      this.props.navigation.dispatch(StackActions.replace('Profile'));
-    } else if (viewId === 'notifications') {
-      this.props.navigation.dispatch(StackActions.replace('Notification'));
-    } else if (viewId === 'offer') {
-      this.props.navigation.dispatch(StackActions.replace('Offers'));
-    }
+  goBack = () => {
+    this.props.navigation.dispatch(StackActions.replace('HomeScreen'));
   };
 
   render() {
     return (
-      <View style={{flex: 5}}>
-        <View style={{flex: 4}}>
-          <Text>LIST</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+          <View style={{flex: 1, left: 10, justifyContent: 'center'}}>
+            <Ionicons
+              style={{elevation: 7}}
+              size={20}
+              name={'md-arrow-round-back'}
+              onPress={() => this.goBack()}
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'flex-end',
+              right: 10,
+              justifyContent: 'center',
+            }}>
+            <Ionicons
+              size={20}
+              name={'md-create'}
+              onPress={() => alert('Dhruv')}
+            />
+          </View>
         </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <TouchableOpacity
-            onPress={() => this.onClickListener('offer')}
-            style={{flex: 1, backgroundColor: 'red'}}
-          />
-          <TouchableOpacity
-            onPress={() => this.onClickListener('profile')}
-            style={{flex: 1, backgroundColor: 'green'}}
-          />
-          <TouchableOpacity
-            onPress={() => this.onClickListener('notifications')}
-            style={{flex: 1, backgroundColor: 'blue'}}
-          />
+        <View style={{flex: 15}}>
+          <View style={{flex: 2}}>
+            <View style={{flex: 3, flexDirection: 'row', left:10}}>
+              <View
+                style={{
+                  flex: 1,
+                  borderRadius: 100,
+                  elevation: 5,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  padding: 20,
+                  marginTop:20,
+                  marginLeft:20,
+                  marginBottom:20,
+                  marginRight:20,
+                }}>
+                <Ionicons
+                  size={60}
+                  name={'md-person'}
+                  onPress={() => this.goBack()}
+                />
+              </View>
+              <View style={{flex: 4, backgroundColor: 'green'}} />
+            </View>
+          </View>
+          <View style={{backgroundColor: 'blue', flex: 2}} />
+          <View style={{flex: 4, backgroundColor: 'red'}} />
         </View>
       </View>
     );
